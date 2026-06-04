dbfe90577efc9ab13b6cd075e4652b860d51a6c0
Sahil kumar singh <sahilkumarsingh749@gmail.com>
2026-06-04 08:17:24 +0530

    Add files via upload

---
+++ voting.java
@@ -0,0 +1,14 @@
+public class voting {
+    public static void main(String[] args){
+        int age = 18;
+        if (age >=17){
+
+        System.out.println("You are eligible to vote");
+    }
+        else {
+            System.out.println("You are not eligible to vote");
+        }
+
+    }
+    
+}
